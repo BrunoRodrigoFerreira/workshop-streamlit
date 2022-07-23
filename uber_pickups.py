43dc7f4d6a0283e85f46da44e227568944e99347
BrunoRodrigoFerreira <66788744+BrunoRodrigoFerreira@users.noreply.github.com>
2022-07-23 14:07:35 -0300

    começando de novo

--- uber_pickups.py
+++ uber_pickups.py
@@ -17,32 +17,7 @@ def load_data(nrows):
     return data
 
 
-# Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
 
-# Load 10,000 rows of data into the dataframe.
-    data = load_data(10000)
-
-# Notify the reader that the data was successfully loaded.
-    data_load_state.text("Done! (using st.cache)")
-    
-    st.subheader('Raw data')
-    st.write(data)
-
-    st.subheader('Number of pickups by hour')
-
-    hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-    st.bar_chart(hist_values)
-
-    st.subheader('Map of all pickups')
-
-    st.map(data)
-    hour_to_filter = 17
-    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-    st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-    st.map(filtered_data)
 
 
   
