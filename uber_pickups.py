6ba56e12bed2b2918d55175f5f702baf0aa9253f
BrunoRodrigoFerreira <66788744+BrunoRodrigoFerreira@users.noreply.github.com>
2022-07-23 13:45:05 -0300

    teste

--- uber_pickups.py
+++ uber_pickups.py
@@ -19,9 +19,19 @@ def load_data(nrows):
 
     # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
+
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
+
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 st.subheader('Raw data')
 st.write(data)
+
+st.subheader('Raw data')
+st.write(data)
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+    st.bar_chart(hist_values)
