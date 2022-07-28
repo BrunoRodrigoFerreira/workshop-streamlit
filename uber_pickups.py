3205fc173dda7d284f9b5e1e0312874e3974fdeb
BrunoRodrigoFerreira <66788744+BrunoRodrigoFerreira@users.noreply.github.com>
2022-07-28 19:12:25 -0300

    teste

--- uber_pickups.py
+++ uber_pickups.py
@@ -16,7 +16,7 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
+# Create a text element and let the reader know the data is loading.0
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
